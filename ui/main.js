e7fc26ebf705f512c8b2bdb797740986eb10f521
Sumit Kumar <sumitk121@yahoo.in>
2016-09-24 17:41:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,32 +5,11 @@ button.onclick = function() {
     
     //create a request object 
     var request = new XMLHttpRequest();
-    
     //capture the response and store it in a variable
     request.onreadystatechange = function(){
       if(request.readyState === XMLHttpRequest.DONE){
        //take some action  
        if(request.status === 200){
-           var counter= request.responseText;
-           var span= document.getElementById('count');
-           span.innerHTML = counter.toString();
-       }
-      }  
-      //not done yet
-    };
-    //make the request
-    
-   request.open('GET','http://sumitk121.imad.hasura-app.io/counter',true);
-   request.send(null);
-   
-   //submit name
-   var nameInput= document.getElementById('name');
-   var name= nameInput.value;
-   var submit= document.getElementById('submit_btn');
-   submit.onclick=function(){
-     //make a request to the server and send the name
-     
-     
      //capture the list of the name and render it as list
      var names= ['name1','name2','name3','name4'];
      var list= '';
@@ -39,6 +18,45 @@ button.onclick = function() {
               }
 var ul= document.getElementById('namelist');
 ul.innerHTML=list;
+       }
+      }  
+      //not done yet
+    };
+    //make the request
+    
+   request.open('GET','http://sumitk121.imad.hasura-app.io/counter',true);
+   request.send(null);
+    
+    
+   //submit name
+   var nameInput= document.getElementById('name');
+   var name= nameInput.value;
+   var submit= document.getElementById('submit_btn');
+   submit.onclick=function(){
+     //make a request to the server and send the name
+     var request = new XMLHttpRequest();
+     //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+       //take some action  
+       if(request.status === 200){
+     //capture the list of the name and render it as list
+     var names=request.responseText;
+     names= JSON.parse(names);
+     var list= '';
+     for(var i=0; i<names.length; i++){
+         list += '<li>' +names[i] +'</li>';
+              }
+var ul= document.getElementById('namelist');
+ul.innerHTML=list;
+       }
+      }  
+      //not done yet
+    };
+    //make the request
+    
+   request.open('GET','http://sumitk121.imad.hasura-app.io/submit-name?name=' + name,true);
+   request.send(null);
      
      
    };
