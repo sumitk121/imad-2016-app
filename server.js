df738f9184605381165ffb3c141eec1d73eddbcd
Sumit Kumar <sumitk121@yahoo.in>
2016-10-24 18:26:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,84 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+
+var articles={       
+    'article-one':{
+    title: 'Article one|Sumit kumar',
+    heading:'Article one',
+    date:'23 oct,2016',
+    content:
+     
+    <p>  My name is Sumit kumar and i study at NAtioal Institute of Technology Jalandhar.</p>
+     
+    <p>
+    I love krishna and his technology and i everyday chant his holy name.
+    </p>
+    <p>
+    I m a devotee of krishna and i love him alot.Hare krishna hare krishna krishna krishna hare hare hare raam hare raam raam raam hare hare.
+    </p>
+       },
+       'article-two':{title: 'Article two|Sumit kumar',
+    heading:'Article two',
+    date:'25 oct,2016',
+    content:
+     
+    <p>  My name is Sumit kumar and i study at NAtioal Institute of Technology Jalandhar.</p>
+     
+    <p>
+    I love krishna and his technology and i everyday chant his holy name.
+    </p>
+    <p>
+    I m a devotee of krishna and i love him alot.Hare krishna hare krishna krishna krishna hare hare hare raam hare raam raam raam hare hare.
+    </p>
+       },
+       'article-three':{title: 'Article three|Sumit kumar',
+    heading:'Article three',
+    date:'27 oct,2016',
+    content:
+     
+    <p>  My name is Sumit kumar and i study at NAtioal Institute of Technology Jalandhar.</p>
+     
+    <p>
+    I love krishna and his technology and i everyday chant his holy name.
+    </p>
+    <p>
+    I m a devotee of krishna and i love him alot.Hare krishna hare krishna krishna krishna hare hare hare raam hare raam raam raam hare hare.
+    </p>
+       },},
+       function create Template(data){
+           var title= data.title;
+           var date=data.date;
+           var heading=data.heading;
+           var content=data.content;
+                  }
+                  var htmlTemplate=
+                  <html>
+                  <head>
+                  <title>
+                  &{title}
+                  </title>
+                  <meta name="viewport" content="width=device-width.initial-scale=1"/>
+                  <link href="/ui/style.css" rel="stylesheet"/>
+                  </head>
+                  <body>
+                  <div class="container">
+                  <div>
+                  <a href="/">HOME</a>
+                  </div>
+                  <hr/>
+                  <h3>
+                  ${heading}
+                  </h3>
+                  <div>
+                  ${date}
+                  </div>
+                  <div>
+                  ${content}
+                  </div>
+                  </body>
+                  </html>
+    app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
